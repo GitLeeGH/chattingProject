8c718d9d96aa62d7b1ba34cdee8ffddf143bd9e0
GitLeeGH <ghlee@di-solution.co.kr>
2022-09-22 13:39:26 +0900

    first upload

---
+++ src/main/java/com/example/chattproject/domain/entity/Member.java
@@ -0,0 +1,26 @@
+package com.example.chattproject.domain.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("Member")
+@Getter
+@Setter
+@Builder
+public class Member {
+
+    @Id
+    private String _id;
+
+    // 가입 이메일
+    private String email;
+
+    // 비밀번호
+    private String password;
+
+    // 지역
+    private String location;
+}
