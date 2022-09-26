5ace1d530ee8d22b34bcefa23a98498a0090e5fd
GitLeeGH <ghlee@di-solution.co.kr>
2022-09-26 21:41:40 +0900

    board

--- src/main/java/com/example/chattproject/controller/BoardController.java
+++ src/main/java/com/example/chattproject/controller/BoardController.java
@@ -6,20 +6,26 @@ import com.example.chattproject.dto.PageResponseDTO;
 import com.example.chattproject.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
-//@RequestMapping("/board")
+@RequestMapping("/board")
 @Log4j2
 @RequiredArgsConstructor
 public class BoardController {
 
     private final BoardService boardService;
 
-    @GetMapping("board/list")
+    @GetMapping("/list")
     public void list(PageRequestDTO pageRequestDTO, Model model){
 
         PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
@@ -29,4 +35,48 @@ public class BoardController {
         model.addAttribute("responseDTO",responseDTO);
 
     }
+
+    /**
+     *  게시글 등록 처리 get 으로 화면 보고 post 방식으로 처리
+     */
+    @GetMapping("/register")
+    public void registerGET(){
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+
+        log.info("board POSt register.............");
+
+        if(bindingResult.hasErrors()) {
+            log.info("has errors.........");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+
+            return "redirect:/board/register";
+        }
+
+        log.info(boardDTO);
+
+        Long bno = boardService.register(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", bno);
+
+        return "redirect:/board/list";
+    }
+
+
+    /**
+     * 게시글 조회
+     */
+    @GetMapping("/read")
+    public void read(Long bno, PageRequestDTO pageRequestDTO, Model model){
+
+        BoardDTO boardDTO = boardService.readOne(bno);
+
+        log.info(boardDTO);
+
+        model.addAttribute("dto",boardDTO);
+    }
+
 }
--- src/main/java/com/example/chattproject/dto/BoardDTO.java
+++ src/main/java/com/example/chattproject/dto/BoardDTO.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -16,10 +18,15 @@ public class BoardDTO {
 
     private Long bno;
 
+    // 제목 내용 작성자 비어있지 않게
+    @NotEmpty
+    @Size(min =3, max =100)
     private String title;
 
+    @NotEmpty
     private String content;
 
+    @NotEmpty
     private String writer;
 
     private LocalDateTime regDate;
