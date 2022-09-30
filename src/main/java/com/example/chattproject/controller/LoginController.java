7fe9ea8191b5d757d15874417bf2d19f9a2f16c5
hrkim <haeryang2@naver.com>
2022-09-30 10:00:33 +0900

    시큐리티 오류 수정

--- src/main/java/com/example/chattproject/controller/LoginController.java
+++ src/main/java/com/example/chattproject/controller/LoginController.java
@@ -1,15 +1,15 @@
-package com.example.chattproject.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class LoginController {
-
-    @GetMapping("/chatlogin")
-    public String goLogin(){
-        System.out.println("login");
-
-        return "/chatlogin";
-    }
-}
+//package com.example.chattproject.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class LoginController {
+//
+//    @GetMapping("/chatlogin")
+//    public String goLogin(){
+//        System.out.println("login");
+//
+//        return "/chatlogin";
+//    }
+//}
---
+++ src/main/java/com/example/chattproject/dao/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.chattproject.dao;
+
+import com.example.chattproject.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
