e3577bd6dbfd19c1f9b8ea759257f7ef84f26151
hrkim <haeryang2@naver.com>
2022-10-05 11:46:44 +0900

    chk

--- src/main/java/com/example/chattproject/exception/RuntimeException.java
+++ src/main/java/com/example/chattproject/exception/RuntimeException.java
@@ -1,27 +1,28 @@
 package com.example.chattproject.exception;
 
-public class RuntimeException extends Exception{
+
+public class RuntimeException extends Exception {
     static final long serialVersionUID = -7034897190745766939L;
 
-    public RuntimeException(){
+    public RuntimeException() {
         super();
     }
 
-    public RuntimeException(String message){
+    public RuntimeException(String message) {
         super(message);
     }
 
-    public RuntimeException(String message, Throwable cause){
+    public RuntimeException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    public RuntimeException(Throwable cause){
+    public RuntimeException(Throwable cause) {
         super(cause);
     }
 
     protected RuntimeException(String message, Throwable cause,
                                boolean enableSuppression,
-                               boolean writableStackTrace){
+                               boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
 }
